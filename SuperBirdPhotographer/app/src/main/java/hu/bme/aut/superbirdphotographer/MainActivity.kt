99d614e78a6659ed2e116a8a4e60b47df8ddf3aa
Kristóf Várszegi <varszegi.kristof99@gmail.com>
2021-09-14 19:33:00 +0200

    feat: create empty android project with compose

---
+++ SuperBirdPhotographer/app/src/main/java/hu/bme/aut/superbirdphotographer/MainActivity.kt
@@ -0,0 +1,37 @@
+package hu.bme.aut.superbirdphotographer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import hu.bme.aut.superbirdphotographer.ui.theme.SuperBirdPhotographerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuperBirdPhotographerTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SuperBirdPhotographerTheme {
+        Greeting("Android")
+    }
+}
