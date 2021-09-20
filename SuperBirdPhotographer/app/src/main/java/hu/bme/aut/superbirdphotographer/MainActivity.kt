e0b8d7aabc0a4d53f1c5c22bae91a73d2e8c160f
Kristóf Várszegi <varszegi.kristof99@gmail.com>
2021-09-20 22:13:41 +0200

    feat(permission request): accompanist-permission setup

--- SuperBirdPhotographer/app/src/main/java/hu/bme/aut/superbirdphotographer/MainActivity.kt
+++ SuperBirdPhotographer/app/src/main/java/hu/bme/aut/superbirdphotographer/MainActivity.kt
@@ -1,90 +1,91 @@
 package hu.bme.aut.superbirdphotographer
 
-import android.Manifest
-import android.graphics.Color
 import android.os.Bundle
-import android.view.ViewGroup.LayoutParams.MATCH_PARENT
-import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.semantics.Role.Companion.Button
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
-import hu.bme.aut.superbirdphotographer.ui.theme.SuperBirdPhotographerTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalPermissionsApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var isPermissionGranted: Boolean? = true
-
-        val launcher =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-                isPermissionGranted = isGranted
-            }
         setContent {
-            SuperBirdPhotographerTheme {
-                Surface(color = MaterialTheme.colors.background) {
+            Surface(color = Color.White, modifier = Modifier.fillMaxSize()) {
+                CameraPermissionRequest(
+                    navigateToSettingsScreen = {},
+                    permissionGrantedComponent = { Text("Permission granteeed camera ready") })
+            }
+        }
+    }
 
-                    when (isPermissionGranted) {
-                        true -> Greeting("")
-                        false -> Text("permission pls")
-                        null -> Button(onClick = { launcher.launch(Manifest.permission.CAMERA) }) {
-                            Text(text = "Start!")
+    @ExperimentalPermissionsApi
+    @Composable
+    private fun CameraPermissionRequest(
+        navigateToSettingsScreen: () -> Unit,
+        permissionGrantedComponent: @Composable () -> Unit
+    ) {
+        var doNotShowRationale by rememberSaveable { mutableStateOf(false) }
+
+        val cameraPermissionState = rememberPermissionState(
+            android.Manifest.permission.CAMERA
+        )
+        when {
+            cameraPermissionState.hasPermission -> {
+                permissionGrantedComponent()
+            }
+            cameraPermissionState.shouldShowRationale ||
+                    !cameraPermissionState.permissionRequested -> {
+                if (doNotShowRationale) {
+                    Text("Feature not available")
+                } else {
+                    AlertDialog(
+                        onDismissRequest = {},
+                        title = { Text("Permission required") },
+                        text = { Text("We need your camera pls") },
+                        confirmButton = {
+                            Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                                Text("Request permission")
+                            }
+                        },
+                        dismissButton = {
+                            Button(onClick = { doNotShowRationale = true }) {
+                                Text("Don't show rationale again")
+                            }
                         }
+                    )
+                }
+            }
+            else -> {
+                Column {
+                    Text(
+                        "Camera permission denied. See this FAQ with information about why we " +
+                                "need this permission. Please, grant us access on the Settings screen."
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Button(onClick = navigateToSettingsScreen) {
+                        Text("Open Settings")
                     }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    val lifecycleOwner = LocalLifecycleOwner.current
-    val context = LocalContext.current
-    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
-    AndroidView(
-        factory = { context ->
-            val previewView = PreviewView(context).apply {
-                setBackgroundColor(Color.GREEN)
-                layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-                scaleType = PreviewView.ScaleType.FILL_START
-                implementationMode = PreviewView.ImplementationMode.COMPATIBLE
-            }
-            val executor = ContextCompat.getMainExecutor(context)
-            cameraProviderFuture.addListener({
-                val cameraProvider = cameraProviderFuture.get()
-                val preview = Preview.Builder().build().also {
-                    it.setSurfaceProvider(previewView.surfaceProvider)
-                }
-
-                val cameraSelector = CameraSelector.Builder()
-                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                    .build()
-
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(
-                    lifecycleOwner,
-                    cameraSelector,
-                    preview
-                )
-            }, executor)
-            previewView
-        },
-        modifier = Modifier.fillMaxSize(),
-    )
 }
