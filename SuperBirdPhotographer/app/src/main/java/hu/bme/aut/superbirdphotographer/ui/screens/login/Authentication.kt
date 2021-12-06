d052695e90524981bb85ca03e338e4285fe27669
Kristóf Várszegi <varszegi.kristof99@gmail.com>
2021-12-06 22:10:20 +0100

    feat(lottie): add lottie animation to login screen

--- SuperBirdPhotographer/app/src/main/java/hu/bme/aut/superbirdphotographer/ui/screens/login/Authentication.kt
+++ SuperBirdPhotographer/app/src/main/java/hu/bme/aut/superbirdphotographer/ui/screens/login/Authentication.kt
@@ -14,11 +14,16 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import hu.bme.aut.superbirdphotographer.R
 
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.background
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.google.android.gms.common.api.ApiException
+import hu.bme.aut.superbirdphotographer.R
 
 
 @ExperimentalMaterialApi
@@ -71,52 +76,54 @@ fun Authentication(
 fun LoginButton(
     action: () -> Unit
 ) {
-    var clicked by remember { mutableStateOf(false) }
-    Surface(
-        onClick = { clicked = !clicked; action() },
-        elevation = 2.dp,
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(start = 8.dp, end = 8.dp, top = 12.dp, bottom = 12.dp)
-                .height(20.dp)
-                .animateContentSize(
-                    animationSpec = tween(
-                        durationMillis = 300,
-                        easing = LinearOutSlowInEasing
-                    )
-                ),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        BirdAnimation()
+        var clicked by remember { mutableStateOf(false) }
+        Surface(
+            onClick = { clicked = !clicked; action() },
+            elevation = 2.dp,
         ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_google_logo),
-                contentDescription = "Google Icon",
-                tint = Color.Unspecified,
+            Row(
                 modifier = Modifier
-                    .width(24.dp)
-                    .height(24.dp)
-            )
-            Spacer(modifier = Modifier.width(18.dp))
-            Text("Sign in with Google", fontWeight = FontWeight.Medium, color = Color.Gray)
-            if (clicked) {
-                Spacer(modifier = Modifier.width(16.dp))
-                CircularProgressIndicator(
+                    .padding(start = 8.dp, end = 8.dp, top = 12.dp, bottom = 12.dp)
+                    .height(20.dp)
+                    .animateContentSize(
+                        animationSpec = tween(
+                            durationMillis = 300,
+                            easing = LinearOutSlowInEasing
+                        )
+                    ),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_google_logo),
+                    contentDescription = "Google Icon",
+                    tint = Color.Unspecified,
                     modifier = Modifier
-                        .height(16.dp)
-                        .width(16.dp),
-                    strokeWidth = 2.dp
+                        .width(24.dp)
+                        .height(24.dp)
                 )
+                Spacer(modifier = Modifier.width(18.dp))
+                Text("Sign in with Google", fontWeight = FontWeight.Medium, color = Color.Gray)
+                if (clicked) {
+                    Spacer(modifier = Modifier.width(16.dp))
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .height(16.dp)
+                            .width(16.dp),
+                        strokeWidth = 2.dp
+                    )
+                }
             }
         }
     }
 }
 
 
-
-@ExperimentalMaterialApi
-@Preview
 @Composable
-fun AuthenticationPreview() {
-    Authentication(navigateToHome = {})
+fun BirdAnimation() {
+    val animationSpec by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.bird_feeding))
+    val progress by animateLottieCompositionAsState(animationSpec, iterations = Integer.MAX_VALUE)
+    LottieAnimation(animationSpec, progress)
 }
